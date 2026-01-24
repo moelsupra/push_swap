2948f62b47b437c5cb5ceec52c913614a0f345b8
moelsupra <medamineelm01@gmail.com>
2026-01-24 18:50:54 +0100

    first commit

---
+++ push_swap.c
@@ -0,0 +1,17 @@
+#include "push_swap.h"
+
+int main(int argc, char **argv)
+{
+    t_node  *stack_a;
+    t_node  *stack_b;
+    char    *joined_args;
+    char    **numbers;
+
+    if (argc < 2)
+        return 0;
+    stack_a = NULL;
+    stack_b = NULL;
+
+    joined_args = joined_arguments(argc, argv);
+    return 0;
+}
---
+++ push_swap.h
@@ -0,0 +1,15 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct s_node
+{
+	int 			value;
+	int 			index;
+	struct s_node	*next;
+}	t_node;
+
+char *join_arguments(int argc, char **argv);
+#endif
